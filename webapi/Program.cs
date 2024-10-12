0ef2ce394e7a59b447e10ad0015c4ae65638843f
fernandofilipuzzi-dev <fernandofilipuzzi.dev@gmail.com>
2024-10-12 15:01:38 +0000

    .

--- webapi/Program.cs
+++ webapi/Program.cs
@@ -10,15 +10,15 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+//if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     //app.UseSwaggerUI();
     // agregado para poder acceder a swagger
     app.UseSwaggerUI(c =>
     {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-        c.RoutePrefix = string.Empty; // deja a swagger disponible en la raiz
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "API V1");
+        c.RoutePrefix = "swagger";//string.Empty; // deja a swagger disponible en la raiz
     });
 }
 
