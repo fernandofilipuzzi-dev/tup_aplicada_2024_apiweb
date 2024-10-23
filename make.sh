50453013d35193edcd12fbaa2c489ce6a9106294
fernandofilipuzzi-dev <fernandofilipuzi.dev@gmail.com>
2024-10-23 00:03:54 +0000

    .

--- make.sh
+++
@@ -1,4 +0,0 @@
-docker stop webapi2
-docker rm webapi2
-docker build --no-cache -f Dockerfile.webapiNetCore -t webapi2:v2.2 .
-docker run --name webapi2 -p 8080:8080  -d webapi2:v2.2
--- runmss.sh
+++
@@ -1,1 +0,0 @@
-docker run -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=MSS-fernando-sql" -p 1433:1433 --name sql2 --hostname sql1 -d mcr.microsoft.com/mssql/server:2022-latest
